cb9ecc89b3878cba3bbdf48740034cc0afb0092a
BrunoKoppel <bruno.koppel@gmail.com>
2020-09-24 22:04:57 -0400

    Initial Commit

---
+++ TournalCLI/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TournalCLI
+//
+//  Created by Bruno Köppel on 9/24/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
